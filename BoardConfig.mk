05cf0492f0e32662701c13da04b6993f69d915cf
devhunter1 <anubhabkundu2@gmail.com>
2024-08-11 05:29:29 +0530

    fix cpu temp always 0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,3 +150,6 @@ TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
 TW_BACKUP_EXCLUSIONS := /data/fonts
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
+
+# CPU temp sysfs path, if it is zero all the time.
+TW_CUSTOM_CPU_TEMP_PATH := true
